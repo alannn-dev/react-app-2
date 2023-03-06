2113e245da8c07cd72ed5f0ab6aff4280d429a98
Alan <boulay.alan@gmail.com>
2023-03-06 16:28:56 +0100

    Add header component with nav

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,41 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledLink } from '../../utils/styles/Atoms'
+import LightLogo from '../../assets/light-logo.png'
+import DarkLogo from '../../assets/dark-logo.png'
+import { useTheme } from '../../utils/hooks'
+
+const HomeLogo = styled.img`
+  height: 70px;
+`
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 function Header() {
+  const { theme } = useTheme()
+
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/42">Questionnaire</Link>
-      <Link to="/freelances">Freelances</Link>
-      <Link to="/results">Header</Link>
-    </nav>
+    <NavContainer>
+      <Link to="/">
+        <HomeLogo src={theme === 'light' ? DarkLogo : LightLogo} />
+      </Link>
+      <div>
+        <StyledLink $theme={theme} to="/">
+          Accueil
+        </StyledLink>
+        <StyledLink $theme={theme} to="/freelances">
+          Profils
+        </StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </div>
+    </NavContainer>
   )
 }
 
