abb05687a0a4143af2a096c9017a9a54eb1d8de2
Alan <boulay.alan@gmail.com>
2023-03-08 14:11:06 +0100

    Add loader for profiles loading

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -87,48 +87,69 @@ const Availability = styled.span`
   padding-left: 20px;
   position: relative;
 `
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 function Profile() {
   const { id: queryId } = useParams()
   const [profileData, setProfileData] = useState({})
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     fetch(`http://localhost:8000/freelance?id=${queryId}`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         setProfileData(jsonResponse?.freelanceData)
+        setLoading(false)
       })
   }, [queryId])
 
   const { picture, name, location, tjm, job, skills, available, id } =
     profileData
 
+  /*  if (loading) {
+    return <Loader />
+  } */
+
   return (
-    <ThemeContext.Consumer>
-      {({ theme }) => (
-        <ProfileWrapper theme={theme}>
-          <Picture src={picture} alt={name} height={150} width={150} />
-          <ProfileDetails theme={theme}>
-            <TitleWrapper>
-              <Title>{name}</Title>
-              <Location>{location}</Location>
-            </TitleWrapper>
-            <JobTitle>{job}</JobTitle>
-            <SkillsWrapper>
-              {skills &&
-                skills.map((skill) => (
-                  <Skill key={`skill-${skill}-${id}`} theme={theme}>
-                    {skill}
-                  </Skill>
-                ))}
-            </SkillsWrapper>
-            <Availability available={available}>
-              {available ? 'Disponible maintenant' : 'Indisponible'}
-            </Availability>
-            <Price>{tjm} € / jour</Price>
-          </ProfileDetails>
-        </ProfileWrapper>
+    <div>
+      {loading ? (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      ) : (
+        <ThemeContext.Consumer>
+          {({ theme }) => (
+            <ProfileWrapper theme={theme}>
+              <Picture src={picture} alt={name} height={150} width={150} />
+              <ProfileDetails theme={theme}>
+                <TitleWrapper>
+                  <Title>{name}</Title>
+                  <Location>{location}</Location>
+                </TitleWrapper>
+                <JobTitle>{job}</JobTitle>
+                <SkillsWrapper>
+                  {skills &&
+                    skills.map((skill) => (
+                      <Skill key={`skill-${skill}-${id}`} theme={theme}>
+                        {skill}
+                      </Skill>
+                    ))}
+                </SkillsWrapper>
+                <Availability available={available}>
+                  {available ? 'Disponible maintenant' : 'Indisponible'}
+                </Availability>
+                <Price>{tjm} € / jour</Price>
+              </ProfileDetails>
+              {/*       )}
+               */}{' '}
+            </ProfileWrapper>
+          )}
+        </ThemeContext.Consumer>
       )}
-    </ThemeContext.Consumer>
+    </div>
   )
 }
 
